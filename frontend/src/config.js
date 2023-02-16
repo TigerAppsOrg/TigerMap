e3ace67b78d8225dc8f4d933c04f854bf197023b
Daniel Park <Danielpark1239@gmail.com>
2023-02-16 15:30:33 -0500

    Changed config URL_PREFIX to deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -27,7 +27,7 @@ const DEFAULT_SEARCH = [
 ];
 
 // DEVELOPMENT: "http://127.0.0.1:5000"
-// DEPLOYMENT: ""
-const URL_PREFIX = "http://localhost:50001"
+// DEPLOYMENT: "https://tigermap.tigerapps.org"
+const URL_PREFIX = "https://tigermap.tigerapps.org"
 
 export { DEFAULT_SEARCH, URL_PREFIX };
